9c6f30d25d80a294c0dd7da95ca24a4e5005ea20
Nick <Nelo-001@mail.ru>
2020-07-01 01:12:01 +0300

    implemented module css and finish screen

--- src/components/WinnerScreen/WinnerScreen.js
+++ src/components/WinnerScreen/WinnerScreen.js
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from './WinnerScreen.module.scss';
+import styles from "./WinnerScreen.module.scss";
 const WinnerScreen = (props) => {
-
   return (
     <div className={styles.WinnerScreen}>
-      <span > winner: {props.winner}</span>
-      <span > Press screen to continue </span>
+      <span> winner: {props.winner}</span>
+      <span> Press screen to continue </span>
     </div>
   );
 };
