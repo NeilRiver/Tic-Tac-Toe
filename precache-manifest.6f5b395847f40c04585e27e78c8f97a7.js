82bb80b060ee7d1dc7850053c440ce9ab80b8f6f
Nick <Nelo-001@mail.ru>
2020-07-05 16:17:13 +0300

    Updates

---
+++ precache-manifest.6f5b395847f40c04585e27e78c8f97a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154ba8b172b35ac01710d201c21e07c8",
+    "url": "/tik-tac-toe/index.html"
+  },
+  {
+    "revision": "9942aab8c45c89429fd8",
+    "url": "/tik-tac-toe/static/css/main.a1a51cef.chunk.css"
+  },
+  {
+    "revision": "75124a70f49dba41a7c4",
+    "url": "/tik-tac-toe/static/js/2.f54da21d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tik-tac-toe/static/js/2.f54da21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9942aab8c45c89429fd8",
+    "url": "/tik-tac-toe/static/js/main.97bdd6eb.chunk.js"
+  },
+  {
+    "revision": "3cfd46c9f49cf16991bf",
+    "url": "/tik-tac-toe/static/js/runtime-main.02f6dce1.js"
+  }
+]);
